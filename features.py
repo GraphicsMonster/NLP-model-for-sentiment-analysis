2e82da77bc161929a4c11e8fb0d387335688a711
Saksham Gupta <www.rameshgupta67@gmail.com>
2023-07-03 18:41:55 +0530

    ngram range added for rich collection of features

--- features.py
+++ features.py
@@ -4,9 +4,12 @@ import pandas as pd
 from preprocess import preprocess_data
 
 def get_features(text):
-    vectorizer = CountVectorizer()
+    vectorizer = CountVectorizer(ngram_range=(1,2))
     features = vectorizer.fit_transform(text)
     return features, vectorizer.get_feature_names_out()
+
+# basically this creates a vocabulary of all the words in the text and then counts the number of times each word appears in each text
+# the output is a matrix of size (number of texts, number of words in vocabulary) and [i,j] entry is the number of times word j appears in text i
 
 
 path = 'Dataset/dataset.csv'
@@ -18,4 +21,4 @@ text, labels = preprocess_data(df)
 X, feature_names = get_features(text)
 
 print(X.shape)
-print(feature_names.shape)
+print("features: ", feature_names)
