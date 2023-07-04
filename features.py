7982477becf41d117ee0f7dd9f597d64d985858d
Saksham Gupta <www.rameshgupta67@gmail.com>
2023-07-04 23:16:02 +0530

    little documentation

--- features.py
+++ features.py
@@ -4,13 +4,20 @@ import pandas as pd
 from preprocess import preprocess_data
 
 def get_features(text):
+
+    # initialize the vectorizer with ngram
     vectorizer = CountVectorizer(ngram_range=(1,2)) 
+
+    # fit the vectorizer on the text
     features = vectorizer.fit_transform(text)
-    return features, vectorizer.get_feature_names_out()
+    # get the vocabulary. This also includes phrases of upto 2 words thanks to ngram_range
+    vocabulary = vectorizer.get_feature_names_out()
+
+    return features, vocabulary
 
 # basically this creates a vocabulary of all the words in the text and then counts the number of times each word appears in each text
 # the output is a matrix of size (number of texts, number of words in vocabulary) and [i,j] entry is the number of times word j appears in text i
-
+# Note: The features matrix must be reshaped to a tensor of size (1, batch_size, vocab_size) in order to be used for further processing
 
 path = 'Dataset/dataset.csv'
 df = pd.read_csv(path)
