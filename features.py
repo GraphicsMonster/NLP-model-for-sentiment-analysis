28c3d644db3e2efc174cc19199a9c6623c198f8e
Saksham Gupta <www.rameshgupta67@gmail.com>
2023-08-14 09:26:26 +0530

    normalization added

--- features.py
+++ features.py
@@ -12,8 +12,16 @@ def get_features(text):
     features = vectorizer.fit_transform(text)
     # get the vocabulary. This also includes phrases of upto 2 words thanks to ngram_range
     vocabulary = vectorizer.get_feature_names_out()
+    # Apply normalization
+    normalized_features = normalize(features.toarray())
+    return normalized_features, vocabulary
 
-    return features, vocabulary
+def normalize(X):
+    # Z-Score normalization
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    X = (X - mean) / std
+    return X
 
 # basically this creates a vocabulary of all the words in the text and then counts the number of times each word appears in each text
 # the output is a matrix of size (number of texts, number of words in vocabulary) and [i,j] entry is the number of times word j appears in text i
